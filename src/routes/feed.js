e4ee3ef3b4fa3441488e74357b355eed06d72993
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2022-09-18 13:49:16 +0530

    Initial commit

---
+++ src/routes/feed.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const feedController = require("../controllers/feed");
+
+router.get("/posts", feedController.getPosts);
+
+router.post("/create", feedController.createPost);
+
+module.exports = router;
