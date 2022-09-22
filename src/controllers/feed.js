68fe9fdcf0aa5ddb8af5e6b89a188a08895477b9
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2022-09-22 12:27:21 +0530

    added pagination

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -7,9 +7,23 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) =>
-      res.status(200).json({ message: "Records fetched", posts, posts })
+      res.status(200).json({
+        message: "Records fetched",
+        posts: posts,
+        totalItems: totalItems,
+      })
     )
     .catch((err) => {
       if (!err.statusCode) {
