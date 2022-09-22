e57928cece728462dd56b21a325f8a3af59c2051
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2022-09-22 12:02:12 +0530

    added update logic

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
@@ -59,4 +63,57 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch((err) => next(err));
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Data is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+  imageUrl = imageUrl.substr(4).replaceAll("\\", "/");
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not found the post");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 404;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => {
+    if (err) console.log(err);
+  });
 };
